fb31b9526596e3cb472948926285b3c1f7f1373b
Alex Roessner <alex.roessner@gmail.com>
2016-10-01 16:25:33 -0600

    Blog So Far

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,16 @@
 #
 
 class User < ApplicationRecord
-  rolify
+  rolify #:before_add => :before_add_method
+  #def before_add_method(role)
+    # do something before it gets added
+  #end
+
+  after_create :assign_default_role
+  def assign_default_role
+    self.add_role(:viewer) if self.roles.blank?
+  end
+
   has_many :comments
   has_many :posts
 end
