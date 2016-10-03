fdcdedf1bd943a97ba8a864fb699d4f9a7a4bd3d
Alex Roessner <alex.roessner@gmail.com>
2016-10-03 15:32:00 -0600

    Fixing Validation Syntax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,7 +13,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates_presence_of :post_id
-  validates_presence_of :body
+  validates :post_id, presence: true
+  validates :body, presence: true
   resourcify
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates_presence_of :title
-  validates_presence_of :body
+  validates :title, presence: true
+  validates :body, presence: true
   resourcify
 end
